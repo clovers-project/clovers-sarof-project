53d609c861903ddaf70ea15967409c9e8ed43a62
KarisAya <1048827424@qq.com>
2024-07-29 12:27:24 +0800

    修改配置

--- clovers_leafgame/modules/game/horse_race/config.py
+++ clovers_leafgame/modules/game/horse_race/config.py
@@ -7,7 +7,7 @@ class Config(BaseModel):
     # 跑道长度
     setting_track_length: int = 30
     # 随机位置事件，能够随机到的跑道范围
-    random_move_range: tuple[float, float] = (0, 0.8)
+    random_move_range: tuple[float, float] = (0.0, 0.8)
     # 每回合基础移动范围
     base_move_range: tuple[int, int] = (1, 3)
     # 事件概率
